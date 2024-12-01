0a84ab83db19c50b51221d7644527960d9e8f365
Illia Likhoshva <ilyaliko64@gmail.com>
2024-12-02 00:24:01 +0200

    Update compile scripts

--- smart_contracts/compile.sh
+++ smart_contracts/compile.sh
@@ -9,15 +9,25 @@ echo "----------------------------------------"
 # Expected location of SmartPy CLI.
 SMART_PY_CLI=~/smartpy-cli/SmartPy.sh
 
+# Expected location of ligo.
+LIGO_CLI=ligo
+
 # Output directory
 OUT_DIR=./.smartpy_out
 
 # Parallel sorted arrays.
-CONTRACTS_ARRAY=(oven-factory dev-fund token stability-fund minter oven-proxy oracle oven oven-registry sandbox-oracle savings-pool)
+CONTRACTS_ARRAY=(oven-factory dev-fund token stability-fund minter oven-proxy oracle oracle-on-demand oven oven-registry sandbox-oracle savings-pool)
 
-# Ensure we have a SmartPy binary.
+CONTRACTS_LIGO_ARRAY=("oracle-on-demand KolibriOracleAdapter")
+
+# # Ensure we have a SmartPy binary.
 if [ ! -f "$SMART_PY_CLI" ]; then
     echo "Fatal: Please install SmartPy CLI at $SMART_PY_CLI" && exit
+fi
+
+# Ensure we have a Ligo binary.
+if [ ! -x "$(command -v $LIGO_CLI)" ]; then
+    echo "Fatal: Please install Ligo CLI, https://ligolang.org/docs/intro/installation" && exit
 fi
 
 # Compile a contract.
@@ -49,6 +59,34 @@ function processContract {
     # TODO(keefertaylor): This is pretty brittle. Consider if we should migrate to Makefile or find a better way.
     cp "$OUT_DIR/${CONTRACT_NAME}/step_000_cont_0_contract.tz" $CONTRACT_OUT || cp "$OUT_DIR/${CONTRACT_NAME}/step_000_cont_1_contract.tz" $CONTRACT_OUT || cp "$OUT_DIR/${CONTRACT_NAME}/step_000_cont_2_contract.tz" $CONTRACT_OUT 
     echo ">>> Written to ${CONTRACT_OUT}"
+
+   
+}
+
+# Compile a ligo contract.
+# Args <contract name, ex: oracle-on-demand> <module, ex: KolibriOracleAdapter>
+function processContractLigo {
+    CONTRACT_NAME=$1
+    OUT_DIR=$2
+    CONTRACT_IN="${CONTRACT_NAME}.jsligo"
+    CONTRACT_OUT="${CONTRACT_NAME}.tz"
+
+    echo ">> Processing ${CONTRACT_NAME}"
+
+    # Ensure file exists.
+    if [ ! -f "$CONTRACT_IN" ]; then
+        echo "Fatal: $CONTRACT_IN not found. Running from wrong dir?" && exit
+    fi
+
+    # Test
+    echo ">>> [1 / 3] Testing ${CONTRACT_NAME} "
+    $LIGO_CLI run test $CONTRACT_IN
+    echo ">>> Done"
+
+    echo ">>> [2 / 3] Compiling ${CONTRACT_NAME}"
+    $LIGO_CLI compile contract $CONTRACT_IN $INVOCATION -o $CONTRACT_OUT
+    echo ">>> Done."
+    echo ">>> Written to ${CONTRACT_OUT}"
 }
 
 echo "> [1 / 3] Unit Testing and Compiling Contracts."
@@ -56,6 +94,13 @@ rm -rf $OUT_DIR
 for i in ${!CONTRACTS_ARRAY[@]}; do
     echo ">> [$((i + 1)) / ${#CONTRACTS_ARRAY[@]}] Processing ${CONTRACTS_ARRAY[$i]}"
     processContract ${CONTRACTS_ARRAY[$i]} ${INVOCATION_ARRAY[$i]} $OUT_DIR
+    echo ">> Done."
+    echo ""
+done
+for i in ${!CONTRACTS_LIGO_ARRAY[@]}; do
+    set -- ${CONTRACTS_LIGO_ARRAY[$i]}
+    echo ">> [$((i + 1)) / ${#CONTRACTS_LIGO_ARRAY[@]}] Processing $1"
+    processContractLigo $1 $2
     echo ">> Done."
     echo ""
 done
